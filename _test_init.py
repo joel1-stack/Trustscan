6a1e6badf758d9236a608222154477da68dade4b
joel1-stack <joelkaunda15@gmail.com>
2026-07-18 10:17:30 +0300

    Clean up test scripts

--- _test_init.py
+++
@@ -1,9 +0,0 @@
-import requests
-base = 'https://trustscan.snapdeploy.dev'
-r = requests.get(base + '/init', timeout=15, allow_redirects=False)
-print(f'Init: {r.status_code}')
-if r.status_code == 200:
-    print(r.text[:500])
-else:
-    loc = r.headers.get('location', '')
-    print(f'Location: {loc}')
